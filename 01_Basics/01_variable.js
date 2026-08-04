de70b9320f8a133f7fb391b4af2281c7f63a2cc9
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-04 13:23:50 +0000

    about variable

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = "123456";
+let accountname = "md faisal";
+var accountbased = "uae"
+let accountinfo;
+
+//accountId = "654321"
+
+console.table([accountId, accountname, accountbased , accountinfo])
