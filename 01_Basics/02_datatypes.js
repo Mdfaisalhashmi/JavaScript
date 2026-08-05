b1c073920bb80de8edeb8387c82aa50236849563
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-05 13:25:16 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // newer version of javascript
+
+//alert(3 + 3) not allowed in node js
+
+console.log(3 + 3)
+
+let name = "md faisal"
+let age = 22
+let isloggedin = true
+
+
+//string
+//number 2 to power 53
+//bigint
+//boolean
+//null
+//undefined
+//symbol
+
+
+//object
+
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
