1676198e096943b8c0e383f83960839fd5c62225
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-07 13:19:26 +0000

    conversion

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let convertinnumber = Number(score)
+console.log(typeof convertinnumber);
+console.log(convertinnumber);
+
+let isloggedin = 1;
+let convertinboolean = Boolean(isloggedin)
+
+console.log(convertinboolean);
+
+
+let somenumber = 35;
+
+let stringnumber = String(somenumber);
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
