08dd000a1dbaa5e980620677a9cd1f207f90ee81
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-08 18:48:03 +0000

    conversionoperation

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -1,23 +1,44 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let convertinnumber = Number(score)
-console.log(typeof convertinnumber);
-console.log(convertinnumber);
+// console.log(typeof convertinnumber);
+// console.log(convertinnumber);
 
 let isloggedin = 1;
 let convertinboolean = Boolean(isloggedin)
 
-console.log(convertinboolean);
+// console.log(convertinboolean);
 
 
 let somenumber = 35;
 
 let stringnumber = String(somenumber);
 
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
 
+let value = 3;
+let negval = -value;
+
+console.log(negval);
+
+let str1 = "hello"
+let str2 = " md faisal"
+let str3 = str1 + str2
+
+
+// console.log(str3)
+
+let gamecount = 100;
+++gamecount
+
+console.log(gamecount);
+
+let gamecountno = 150;
+gamecountno++
+
+console.log(gamecountno);
