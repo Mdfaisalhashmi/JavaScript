92a74114a268a3151a19aedb07f0a27697712376
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-12 13:09:48 +0000

    nums

---
+++ 01_Basics/06_num_and_maths.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score)
+
+
+const balnace = new Number(400)
+console.log(balnace)
+
+console.log(balnace.toString().length)
+console.log(balnace.toFixed(2))
+
+const othernumber = 123.5688
+console.log(othernumber.toPrecision(4));
+
+const hundred = 1000000;
+console.log(hundred.toLocaleString('en-IN'));
+
