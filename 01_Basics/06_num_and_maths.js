a66edda3aba2134618110256125264d9c38c6665
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-13 13:05:35 +0000

    maths

--- 01_Basics/06_num_and_maths.js
+++ 01_Basics/06_num_and_maths.js
@@ -1,16 +1,34 @@
 const score = 400
-console.log(score)
+//console.log(score)
 
 
 const balnace = new Number(400)
-console.log(balnace)
+//console.log(balnace)
 
-console.log(balnace.toString().length)
-console.log(balnace.toFixed(2))
+// console.log(balnace.toString().length)
+// console.log(balnace.toFixed(2))
 
 const othernumber = 123.5688
-console.log(othernumber.toPrecision(4));
+//console.log(othernumber.toPrecision(4));
 
 const hundred = 1000000;
-console.log(hundred.toLocaleString('en-IN'));
+//console.log(hundred.toLocaleString('en-IN'));
 
+// maths---------------------------------------------------------------------
+
+console.log(Math)
+console.log(Math.PI);
+console.log(Math.round(4.7))
+console.log(Math.abs(-4))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(3.2))
+console.log(Math.min(4,6,10,15))
+console.log(Math.max(8, 9 ,6, 12));
+
+console.log(Math.random())
+console.log((Math.floor(Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
