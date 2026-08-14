8d29e52fd787993c4cf65566b70de740f7d58bd1
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-14 13:32:56 +0000

    dates in js

---
+++ 01_Basics/07_datesinjs.js
@@ -0,0 +1,28 @@
+let date = new Date()
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toJSON())
+// console.log(date.toDateString())
+// console.log(date.toISOString())
+// console.log(date.toLocaleString())
+
+let mycreateddate = new Date(2023 , 3 , 23 , 5 , 3)
+//console.log(mycreateddate.toLocaleString());
+
+
+let timestamp = Date.now()
+console.log(timestamp);
+
+
+//console.log(mycreateddate.getTime());
+
+console.log(Math.floor(Date.now() / 1000));
+
+let newdate = new Date()
+
+console.log(newdate);
+console.log(newdate.getDay());
+console.log(newdate.getMonth());
+
+
