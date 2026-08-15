cd90e7c6ca3583c4bb4601dcd904f7f7b784f10a
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-15 19:39:43 +0000

    arrays part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,32 @@
+const myArr = [0 , 1 , 2, 3, 4]
+
+//console.log(myArr[4])
+
+const myArr2 = new Array(1 , 2 , 3, 4)
+//console.log(myArr2)
+
+// myArr.push(6)
+// myArr.push("world")
+// //myArr.pop()
+// myArr.unshift(22)
+// myArr.shift()
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice,  splice
+
+const mynewArr = myArr.slice(1 , 3)
+console.log(mynewArr);
+console.log(myArr);
+
+
+const mynewArr2 = myArr.splice(1 , 3)
+console.log(mynewArr2)
+console.log(myArr);
+
