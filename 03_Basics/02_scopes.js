8a7d2990352789e3278ae920392905ec25c27795
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-22 19:24:45 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,56 @@
+
+let a = 300;
+
+if (true){
+
+let a = 10;
+const b  = 20;
+var c = 30;
+
+//console.log("inner: " , a);
+
+}
+
+for(let i = 0; i < Array.length ; i++){
+  const element = Array[i]
+}
+
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+function one(){
+  username = "mdfaisalhashmi"
+
+  function two(){
+    websites = "youtube.com"
+    console.log(username)
+  }
+  two()
+}
+
+
+if(true){   
+  const username = "mdfaisalhashmi" 
+
+  if(username === "mdfaisalhashmi"){
+     const website = " youtube"
+    // console.log(username + website)
+  }
+  //console.log(website)
+}
+
+//console.log(username)
+
+console.log(addone(5))
+
+function addone(num){
+  return num + 1
+}
+
+addtwo(2)
+const addtwo = function(num){
+  return num + 2
+}
+
+
