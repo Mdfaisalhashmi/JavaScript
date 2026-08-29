1722e531492f8555f973c4263aa81708a79b5553
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-29 13:38:45 +0000

    iife

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,13 @@
+// IIFE Immeditely invocked function : iife
+
+//named iife
+
+(function hello(){
+    console.log("hello md faisal");
+    
+})();
+
+//unnamed iife
+( (age) => {
+    console.log(`hello hashmi ${25}`)
+})(25);
