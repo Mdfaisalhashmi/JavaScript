59a93c89fffaff411b484752d8cc3fe75bf3373f
Mdfaisalhashmi <126041327+Mdfaisalhashmi@users.noreply.github.com>
2026-08-29 13:39:06 +0000

    if else

---
+++ 04_controlflow/one.js
@@ -0,0 +1,56 @@
+// //const userloggedin = false
+// const temprature = 51;
+
+// if (temprature < 50){
+//     console.log("temprature is less than 50")
+// }
+// else{
+//     console.log("temprature is greater than 50")
+// }
+
+//console.log("ëxecauted");
+
+// const score = 200;
+
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`user power :${power}`);
+    
+// }
+
+//console.log(`user power :${power}`);
+
+// const balance = 1000;
+
+// if (balance > 500) console.log("hellow test"),
+// console.log("hello test2");
+
+// balance = 1400;
+
+// if(balance < 100){
+//    console.log("balance is less than 100");
+   
+// }
+// else if(balance < 500){
+//     console.log("balance is less than 500")
+// }
+// else{
+//     console.log("balance is greater than 1400");
+    
+// }
+
+const isuserloggedin = true
+const isdebitcard = true
+const userloggedinfromgoogle = false
+const userloggedinfromemail = false
+
+if (isuserloggedin && isdebitcard && 2 == 4) {
+    console.log("allow to purchase course");
+    
+}
+
+if (userloggedinfromemail || userloggedinfromgoogle) {
+    console.log("allow to purchase course");
+}
+
+
